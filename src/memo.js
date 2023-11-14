11f709ab277276f7810f5a504a13ee6b583c9b36
farwayer <farwayer@gmail.com>
2023-11-14 16:53:32 +0100

    add react/memo

---
+++ src/memo.js
@@ -0,0 +1,38 @@
+import {onRead, onWrite} from './store.js'
+
+
+let memo = (store, calc) => {
+  let cached = false
+  let cachedResult
+  let deps = new WeakMap()
+
+  onWrite(store, (obj, prop) => {
+    let props = deps.get(obj)
+    if (!props) return
+    if (!props.has(prop)) return
+
+    cached = false
+  })
+
+  onRead(store, (obj, prop) => {
+    let props = deps.get(obj)
+
+    if (props) {
+      props.add(prop)
+    } else {
+      deps.set(obj, new Set([prop]))
+    }
+  })
+
+  return () => {
+    if (cached) {
+      return cachedResult
+    }
+
+    deps = new WeakMap()
+    cachedResult = calc()
+    cached = true
+
+    return cachedResult
+  }
+}
