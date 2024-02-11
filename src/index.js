c6008f2771939748e6d7347fd77393dfff49c9d9
farwayer <farwayer@gmail.com>
2024-02-11 17:49:29 +0100

    missed index files

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export * from './store.js'
+export * from './proxify/index.js'
+export * from './proxify/ref.js'
---
+++ src/react/index.js
@@ -0,0 +1,3 @@
+export * from './use-store.js'
+export * from './use-derived.js'
+export * from './select.js'
