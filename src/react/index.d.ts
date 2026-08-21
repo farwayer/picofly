3e993312bd14dc9511b6a0d59ca0853fc1f5c4ad
farwayer <farwayer@gmail.com>
2026-08-21 12:25:31 +0200

    react: improve ts types, show prop names in errors

--- src/react/index.d.ts
+++ src/react/index.d.ts
@@ -14,95 +14,121 @@ export type SelectOptions<S> = {
   withRef?: boolean
 }
 
+type None = null | undefined | false | 0 | 0n | ''
+
+type Norm<E> =
+  [E] extends [void] ? {} :
+  [Extract<E, None>] extends [never] ? E :
+  [Exclude<E, None>] extends [never] ? {} :
+  Partial<Exclude<E, None>>
+
+type OptionalKeys<T> = {
+  [K in keyof T]-?: {} extends Pick<T, K> ? K : never
+}[keyof T]
+
+type Mismatch<PE, FP> = {
+  [K in keyof PE & keyof FP]: PE[K] extends FP[K] ? never : K
+}[keyof PE & keyof FP]
+
 export type Select<S, PE> = <FP>(
-  Component: ComponentType<FP> & ([PE] extends [void] ? unknown : [Exclude<keyof PE, keyof FP>] extends [never] ? [PE] extends [Pick<FP, keyof PE>] ? unknown : never : never),
+  Component: ComponentType<FP> & (
+    [Exclude<keyof PE, keyof FP>] extends [never]
+      ? [Required<PE>] extends [Pick<FP, keyof PE & keyof FP>]
+        ? unknown
+        : {'select: props type mismatch': Mismatch<Required<PE>, FP>}
+      : {'select: props missing in component': Exclude<keyof PE, keyof FP>}
+  ),
   options?: SelectOptions<S>,
-) => FunctionComponent<Omit<FP, keyof PE>>
+) => FunctionComponent<
+  [OptionalKeys<PE>] extends [never]
+    ? Omit<FP, keyof PE>
+    : Omit<FP, keyof PE> & Partial<Pick<FP, OptionalKeys<PE> & keyof FP>>
+>
 
 export function select<S>(
 ): Select<S, {}>
 
 export function select<S, P0, E0>(
   s0: Selector<S, P0, E0>,
-): Select<S, E0>
+): Select<S, Norm<E0>>
 
 export function select<S, P0, E0, E1>(
   s0: Selector<S, P0, E0>,
-  s1: Selector<S, P0 & E0, E1>,
-): Select<S, E0 & E1>
+  s1: Selector<S, P0 & Norm<E0>, E1>,
+): Select<S, Norm<E0> & Norm<E1>>
 
 export function select<S, P0, E0, E1, E2>(
   s0: Selector<S, P0, E0>,
-  s1: Selector<S, P0 & E0, E1>,
-  s2: Selector<S, P0 & E0 & E1, E2>,
-): Select<S, E0 & E1 & E2>
+  s1: Selector<S, P0 & Norm<E0>, E1>,
+  s2: Selector<S, P0 & Norm<E0> & Norm<E1>, E2>,
+): Select<S, Norm<E0> & Norm<E1> & Norm<E2>>
 
 export function select<S, P0, E0, E1, E2, E3>(
   s0: Selector<S, P0, E0>,
-  s1: Selector<S, P0 & E0, E1>,
-  s2: Selector<S, P0 & E0 & E1, E2>,
-  s3: Selector<S, P0 & E0 & E1 & E2, E3>,
-): Select<S, E0 & E1 & E2 & E3>
+  s1: Selector<S, P0 & Norm<E0>, E1>,
+  s2: Selector<S, P0 & Norm<E0> & Norm<E1>, E2>,
+  s3: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2>, E3>,
+): Select<S, Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3>>
 
 export function select<S, P0, E0, E1, E2, E3, E4>(
   s0: Selector<S, P0, E0>,
-  s1: Selector<S, P0 & E0, E1>,
-  s2: Selector<S, P0 & E0 & E1, E2>,
-  s3: Selector<S, P0 & E0 & E1 & E2, E3>,
-  s4: Selector<S, P0 & E0 & E1 & E2 & E3, E4>,
-): Select<S, E0 & E1 & E2 & E3 & E4>
+  s1: Selector<S, P0 & Norm<E0>, E1>,
+  s2: Selector<S, P0 & Norm<E0> & Norm<E1>, E2>,
+  s3: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2>, E3>,
+  s4: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3>, E4>,
+): Select<S, Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4>>
 
 export function select<S, P0, E0, E1, E2, E3, E4, E5>(
   s0: Selector<S, P0, E0>,
-  s1: Selector<S, P0 & E0, E1>,
-  s2: Selector<S, P0 & E0 & E1, E2>,
-  s3: Selector<S, P0 & E0 & E1 & E2, E3>,
-  s4: Selector<S, P0 & E0 & E1 & E2 & E3, E4>,
-  s5: Selector<S, P0 & E0 & E1 & E2 & E3 & E4, E5>,
-): Select<S, E0 & E1 & E2 & E3 & E4 & E5>
+  s1: Selector<S, P0 & Norm<E0>, E1>,
+  s2: Selector<S, P0 & Norm<E0> & Norm<E1>, E2>,
+  s3: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2>, E3>,
+  s4: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3>, E4>,
+  s5: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4>, E5>,
+): Select<S, Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5>>
 
 export function select<S, P0, E0, E1, E2, E3, E4, E5, E6>(
   s0: Selector<S, P0, E0>,
-  s1: Selector<S, P0 & E0, E1>,
-  s2: Selector<S, P0 & E0 & E1, E2>,
-  s3: Selector<S, P0 & E0 & E1 & E2, E3>,
-  s4: Selector<S, P0 & E0 & E1 & E2 & E3, E4>,
-  s5: Selector<S, P0 & E0 & E1 & E2 & E3 & E4, E5>,
-  s6: Selector<S, P0 & E0 & E1 & E2 & E3 & E4 & E5, E6>,
-): Select<S, E0 & E1 & E2 & E3 & E4 & E5 & E6>
+  s1: Selector<S, P0 & Norm<E0>, E1>,
+  s2: Selector<S, P0 & Norm<E0> & Norm<E1>, E2>,
+  s3: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2>, E3>,
+  s4: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3>, E4>,
+  s5: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4>, E5>,
+  s6: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5>, E6>,
+): Select<S, Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5> & Norm<E6>>
 
 export function select<S, P0, E0, E1, E2, E3, E4, E5, E6, E7>(
   s0: Selector<S, P0, E0>,
-  s1: Selector<S, P0 & E0, E1>,
-  s2: Selector<S, P0 & E0 & E1, E2>,
-  s3: Selector<S, P0 & E0 & E1 & E2, E3>,
-  s4: Selector<S, P0 & E0 & E1 & E2 & E3, E4>,
-  s5: Selector<S, P0 & E0 & E1 & E2 & E3 & E4, E5>,
-  s6: Selector<S, P0 & E0 & E1 & E2 & E3 & E4 & E5, E6>,
-  s7: Selector<S, P0 & E0 & E1 & E2 & E3 & E4 & E5 & E6, E7>,
-): Select<S, E0 & E1 & E2 & E3 & E4 & E5 & E6 & E7>
+  s1: Selector<S, P0 & Norm<E0>, E1>,
+  s2: Selector<S, P0 & Norm<E0> & Norm<E1>, E2>,
+  s3: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2>, E3>,
+  s4: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3>, E4>,
+  s5: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4>, E5>,
+  s6: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5>, E6>,
+  s7: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5> & Norm<E6>, E7>,
+): Select<S, Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5> & Norm<E6> & Norm<E7>>
 
 export function select<S, P0, E0, E1, E2, E3, E4, E5, E6, E7, E8>(
   s0: Selector<S, P0, E0>,
-  s1: Selector<S, P0 & E0, E1>,
-  s2: Selector<S, P0 & E0 & E1, E2>,
-  s3: Selector<S, P0 & E0 & E1 & E2, E3>,
-  s4: Selector<S, P0 & E0 & E1 & E2 & E3, E4>,
-  s5: Selector<S, P0 & E0 & E1 & E2 & E3 & E4, E5>,
-  s6: Selector<S, P0 & E0 & E1 & E2 & E3 & E4 & E5, E6>,
-  s7: Selector<S, P0 & E0 & E1 & E2 & E3 & E4 & E5 & E6, E7>,
-  s8: Selector<S, P0 & E0 & E1 & E2 & E3 & E4 & E5 & E6 & E7, E8>,
-): Select<S, E0 & E1 & E2 & E3 & E4 & E5 & E6 & E7 & E8>
+  s1: Selector<S, P0 & Norm<E0>, E1>,
+  s2: Selector<S, P0 & Norm<E0> & Norm<E1>, E2>,
+  s3: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2>, E3>,
+  s4: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3>, E4>,
+  s5: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4>, E5>,
+  s6: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5>, E6>,
+  s7: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5> & Norm<E6>, E7>,
+  s8: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5> & Norm<E6> & Norm<E7>, E8>,
+): Select<S, Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5> & Norm<E6> & Norm<E7> & Norm<E8>>
 
 export function select<S, P0, E0, E1, E2, E3, E4, E5, E6, E7, E8, E9>(
   s0: Selector<S, P0, E0>,
-  s1: Selector<S, P0 & E0, E1>,
-  s2: Selector<S, P0 & E0 & E1, E2>,
-  s3: Selector<S, P0 & E0 & E1 & E2, E3>,
-  s4: Selector<S, P0 & E0 & E1 & E2 & E3, E4>,
-  s5: Selector<S, P0 & E0 & E1 & E2 & E3 & E4, E5>,
-  s6: Selector<S, P0 & E0 & E1 & E2 & E3 & E4 & E5, E6>,
-  s7: Selector<S, P0 & E0 & E1 & E2 & E3 & E4 & E5 & E6, E7>,
-  s8: Selector<S, P0 & E0 & E1 & E2 & E3 & E4 & E5 & E6 & E7, E8>,
-  s9: Selector<S, P0 & E0 & E1 & E2 & E3 & E4 & E5 & E6 & E7 & E8, E9>,
-): Select<S, E0 & E1 & E2 & E3 & E4 & E5 & E6 & E7 & E8 & E9>
+  s1: Selector<S, P0 & Norm<E0>, E1>,
+  s2: Selector<S, P0 & Norm<E0> & Norm<E1>, E2>,
+  s3: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2>, E3>,
+  s4: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3>, E4>,
+  s5: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4>, E5>,
+  s6: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5>, E6>,
+  s7: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5> & Norm<E6>, E7>,
+  s8: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5> & Norm<E6> & Norm<E7>, E8>,
+  s9: Selector<S, P0 & Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5> & Norm<E6> & Norm<E7> & Norm<E8>, E9>,
+): Select<S, Norm<E0> & Norm<E1> & Norm<E2> & Norm<E3> & Norm<E4> & Norm<E5> & Norm<E6> & Norm<E7> & Norm<E8> & Norm<E9>>
