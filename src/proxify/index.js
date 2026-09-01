b53d961d1621e8957d5e5bd6163e381cfeea5747
farwayer <farwayer@gmail.com>
2026-09-02 01:51:31 +0200

    proxify: as expressions, trimed bytes

--- src/proxify/index.js
+++ src/proxify/index.js
@@ -5,143 +5,97 @@ import {RefSym} from './ref.js'
 
 export let obj = ($, val) =>
 	typeof val === 'object' && val
-		? $[1].get(val) ?? proxifyObj($, val)
+		? $[1].get(val) || proxifyObj($, val)
 		: val
 
-export let objIgnoreSpecials = ($, val) => {
-	if (typeof val !== 'object' || !val) {
-		return val
-	}
-
-	let proxy = $[1].get(val)
-	if (proxy) {
-		return proxy
-	}
-
-	return (
-		Symbol.toStringTag in val ||
-		val instanceof Date ||
-		val instanceof Error ||
-		val instanceof RegExp ||
-		val instanceof Number ||
-		val instanceof String ||
-		val instanceof Boolean
-	)
+export let objIgnoreSpecials = ($, val) =>
+	typeof val !== 'object' || !val
 		? val
-		: proxifyObj($, val)
-}
+		: $[1].get(val) || (
+			Symbol.toStringTag in val ||
+			val instanceof Date ||
+			val instanceof Error ||
+			val instanceof RegExp ||
+			val instanceof Number ||
+			val instanceof String ||
+			val instanceof Boolean
+				? val
+				: proxifyObj($, val)
+		)
 
 export let map = ($, val) =>
 	val instanceof Map
-		? $[1].get(val) ?? proxifyMap($, val)
+		? $[1].get(val) || proxifyMap($, val)
 		: val
 
-export let objMap = ($, val) => {
-	if (typeof val !== 'object' || !val) {
-		return val
-	}
-
-	let proxy = $[1].get(val)
-	if (proxy) {
-		return proxy
-	}
-
-	return val instanceof Map
-		? proxifyMap($, val)
-		: proxifyObj($, val)
-}
-
-export let objMapIgnoreSpecials = ($, val) => {
-	if (typeof val !== 'object' || !val) {
-		return val
-	}
-
-	let proxy = $[1].get(val)
-	if (proxy) {
-		return proxy
-	}
-
-	if (val instanceof Map) {
-		return proxifyMap($, val)
-	}
-
-	return (
-		Symbol.toStringTag in val ||
-		val instanceof Date ||
-		val instanceof Error ||
-		val instanceof RegExp ||
-		val instanceof Number ||
-		val instanceof String ||
-		val instanceof Boolean
-	)
+export let objMap = ($, val) =>
+	typeof val !== 'object' || !val
 		? val
-		: proxifyObj($, val)
-}
+		: $[1].get(val) || (
+			val instanceof Map
+				? proxifyMap($, val)
+				: proxifyObj($, val)
+		)
 
-export let objMapIgnoreSpecialsRef = ($, val) => {
-	if (typeof val !== 'object' || !val) {
-		return val
-	}
-
-	let proxy = $[1].get(val)
-	if (proxy) {
-		return proxy
-	}
-
-	let refs = $[RefSym]
-	if (refs && refs.has(val)) {
-		return val
-	}
-
-	if (val instanceof Map) {
-		return proxifyMap($, val)
-	}
-
-	return (
-		Symbol.toStringTag in val ||
-		val instanceof Date ||
-		val instanceof Error ||
-		val instanceof RegExp ||
-		val instanceof Number ||
-		val instanceof String ||
-		val instanceof Boolean
-	)
+export let objMapIgnoreSpecials = ($, val) =>
+	typeof val !== 'object' || !val
 		? val
-		: proxifyObj($, val)
-}
+		: $[1].get(val) || (
+			val instanceof Map
+				? proxifyMap($, val)
+				: (
+					Symbol.toStringTag in val ||
+					val instanceof Date ||
+					val instanceof Error ||
+					val instanceof RegExp ||
+					val instanceof Number ||
+					val instanceof String ||
+					val instanceof Boolean
+						? val
+						: proxifyObj($, val)
+				)
+		)
 
-export let objMapSetIgnoreSpecialsRef = ($, val) => {
-	if (typeof val !== 'object' || !val) {
-		return val
-	}
-
-	let proxy = $[1].get(val)
-	if (proxy) {
-		return proxy
-	}
-
-	let refs = $[RefSym]
-	if (refs && refs.has(val)) {
-		return val
-	}
-
-	if (val instanceof Map) {
-		return proxifyMap($, val)
-	}
-
-	if (val instanceof Set) {
-		return proxifySet($, val)
-	}
-
-	return (
-		Symbol.toStringTag in val ||
-		val instanceof Date ||
-		val instanceof Error ||
-		val instanceof RegExp ||
-		val instanceof Number ||
-		val instanceof String ||
-		val instanceof Boolean
-	)
+export let objMapIgnoreSpecialsRef = ($, val) =>
+	typeof val !== 'object' || !val
 		? val
-		: proxifyObj($, val)
-}
+		: $[1].get(val) || (
+			$[RefSym]?.has(val)
+				? val
+				: val instanceof Map
+					? proxifyMap($, val)
+					: (
+						Symbol.toStringTag in val ||
+						val instanceof Date ||
+						val instanceof Error ||
+						val instanceof RegExp ||
+						val instanceof Number ||
+						val instanceof String ||
+						val instanceof Boolean
+							? val
+							: proxifyObj($, val)
+					)
+		)
+
+export let objMapSetIgnoreSpecialsRef = ($, val) =>
+	typeof val !== 'object' || !val
+		? val
+		: $[1].get(val) || (
+			$[RefSym]?.has(val)
+				? val
+				: val instanceof Map
+					? proxifyMap($, val)
+					: val instanceof Set
+						? proxifySet($, val)
+						: (
+							Symbol.toStringTag in val ||
+							val instanceof Date ||
+							val instanceof Error ||
+							val instanceof RegExp ||
+							val instanceof Number ||
+							val instanceof String ||
+							val instanceof Boolean
+								? val
+								: proxifyObj($, val)
+						)
+		)
