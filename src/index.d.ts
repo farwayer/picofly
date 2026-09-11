28773a6a21a38cc12790260423fa748fdeabd6f4
farwayer <farwayer@gmail.com>
2026-09-11 07:53:07 +0200

    ts: fix lock/unlock return, export I* internal state indexes

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,15 +4,15 @@ export function markRaw<S, V>(store: S, val: V): V
 export function isRaw<S, V>(store: S, val: V): boolean
 export function onWrite<S>(store: S, cb: Sub): Unsub
 export function onRead<S>(store: S, cb: Sub): Unsub
-export function lock<S>(store: S): void
-export function unlock<S>(store: S): void
+export function lock<S>(store: S): Locked
+export function unlock<S>(store: S): Locked
 export function isLocked<S>(store: S): boolean
 
-export const raw: Rule
-export const map: Rule
-export const set: Rule
-export const builtins: Rule
-export const obj: Rule
+export const raw: Rule // priority: 10
+export const map: Rule // priority: 20
+export const set: Rule // priority: 30
+export const builtins: Rule // priority: 40
+export const obj: Rule // priority: 50
 
 export type Sub = (obj: object, key: unknown) => void
 export type Unsub = () => void
@@ -20,7 +20,7 @@ export type Unsub = () => void
 // takes the value or passes it to the next step
 export type Step = ($: $, val: any) => any
 
-// returns priority if called without the next step
+// must return priority if called without the next
 export type Rule = (next?: Step) => number | Step
 
 
@@ -34,6 +34,15 @@ export function get$<S>(store: S): $
 export function naked<V>($: $, val: V): V
 
 export type Locked = 1 | 0
+
+export const IProxify: number
+export const IWriteSubs: number
+export const IReadSubs: number
+export const ICache: number
+export const ILocked: number
+
+// don't rely on the sequence: it may be changed from version to version
+// use I* indexes
 export type $ = [
 	proxify: Step,
 	writeSubs: Set<Sub>,
