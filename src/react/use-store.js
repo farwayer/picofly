99f54bfb0abc05ab13703cd290ac7b9ff41c4250
farwayer <farwayer@gmail.com>
2026-09-11 14:01:12 +0200

    react: atomic subscribe version WIP

--- src/react/use-store.js
+++ src/react/use-store.js
@@ -6,73 +6,147 @@ import {onWrite, onRead, lock, unlock, get$} from '../store.js'
 
 export let PicoflyContext
 export let Picofly = /* @__PURE__ */ (() => (
-	PicoflyContext ||= createContext()
+	PicoflyContext ??= createContext()
 ).Provider)()
 export let useContextStore = () => useContext(PicoflyContext)
 
 export let useStore = (store = useContextStore() || "use <Picofly>"()) => {
-	let trackedRef = useRef()
-	let updateIdRef = useRef(0)
+	// store will be locked to change immediately after the call
+	// and unlocked at any (!) component commit stage
+	lock(store)
 
+	let epochRef = useRef(0)
+	let trackedRef = useRef()
+	let writeUnsubsRef = useRef()
+	let onChangeRef = useRef()
+
+	let $ = get$(store)
+	let $react = $[ReactSym]
+
+	writeUnsubsRef.current ??= new Set()
+
+
+	// init (once per component)
+	// epoch used as inited flag
+	if (!epochRef.current) {
+		epochRef.current++
+
+		// attach the store write broker (once: one for all components)
+		if (!$react) {
+			let brokerSubs = new WeakMap()
+
+			let brokerUnsub = onWrite(store, (obj, key) => {
+				let objSubs = brokerSubs.get(obj)
+				if (!objSubs?.size) return
+
+				for (let cb of objSubs) {
+					cb(obj, key)
+				}
+			})
+
+			$react = $[ReactSym] = {
+				alive: 0,
+				brokerSubs,
+				brokerUnsub,
+			}
+		}
+
+		$react.alive++
+	}
+
+	let brokerRelease = () => {
+		if (!--$react.alive) {
+			$react.brokerUnsub()
+			delete $[ReactSym]
+		}
+	}
+
+
+	// tracking
+	let stopTrackRead = () => {
+		if ($react.readUnsub) {
+			$react.readUnsub()
+			$react.readUnsub = null
+			unlock(store)
+		}
+	}
+
+	let stopTrackWrite = () => {
+		let unsubs = writeUnsubsRef.current
+		if (!unsubs.size) return
+
+		for (let unsub of unsubs) {
+			unsub()
+		}
+		unsubs.clear()
+	}
+
+	let notifyIfTrackedKey = (obj, key) => {
+		if (trackedRef.current.get(obj)?.has(key)) {
+			stopTrackWrite()
+			epochRef.current++
+			onChangeRef.current()
+		}
+	}
+
+	let trackKey = (obj, key) => {
+		let tracked = trackedRef.current
+		let trackedKeys = tracked.get(obj)
+
+		if (trackedKeys) {
+			trackedKeys.add(key)
+		} else {
+			tracked.set(obj, new Set().add(key))
+
+			let unsub = onObjWrite($react, obj, notifyIfTrackedKey)
+			writeUnsubsRef.current.add(unsub)
+		}
+	}
+
+	stopTrackWrite()
 	trackedRef.current = new WeakMap()
 
-	useRenderRead(store, (obj, key) => {
-		let tracked = trackedRef.current
-		let objTrackedKeys = tracked.get(obj)
+	// remove previous component read callback (if exists) and attach our
+	$react.readUnsub?.()
+	$react.readUnsub = onRead(store, trackKey)
 
-		if (objTrackedKeys) {
-			objTrackedKeys.add(key)
-		} else {
-			tracked.set(obj, new Set([key]))
+	let subscribe = useCallback(onChange => {
+		onChangeRef.current = onChange
+
+		return () => {
+			stopTrackWrite()
+			brokerRelease()
 		}
-	})
+	}, [$react])
 
-	// 79 bc
-	let subscribe = useCallback(onChange => onWrite(store, (obj, key) => {
-		if (trackedRef.current?.get(obj)?.has(key)) {
-			trackedRef.current = null
-			updateIdRef.current++
-			onChange()
-		}
-	}), [store])
+	let getEpoch = () => epochRef.current
 
-	let getUpdateId = () => (
-		updateIdRef.current
-	)
+	useSyncExternalStore(subscribe, getEpoch, getEpoch)
+	useInsertionEffect(stopTrackRead)
 
-	useSyncExternalStore(subscribe, getUpdateId)
+	// due to the asynchronous nature of rendering
+	// useInsertionEffect may not always be called after each render
+	// (for ex. when the data was updated between the render and commit stages)
+	// we should schedule cleanup so as not to miss such a situation
+	queueMicrotask(stopTrackRead)
 
 	return store
 }
 
 // private
-let RenderReadUnsubSym = Symbol()
+let ReactSym = Symbol()
 
-// store will be locked to change immediately after the call
-// and unlocked at any (!) component commit stage
-// keep in mind that next rendered component
-// will override previous onRead callback
-let useRenderRead = (store, cb) => {
-	lock(store)
+let onObjWrite = ($react, obj, cb) => {
+	let brokerSubs = $react.brokerSubs
+	let objSubs = brokerSubs.get(obj)
 
-	let $ = get$(store)
-
-	$[RenderReadUnsubSym]?.()
-	$[RenderReadUnsubSym] = onRead(store, cb)
-
-	let cleanup = () => {
-		if (!$[RenderReadUnsubSym]) return
-
-		$[RenderReadUnsubSym]()
-		$[RenderReadUnsubSym] = null
-		unlock(store)
+	if (objSubs) {
+		objSubs.add(cb)
+	} else {
+		$react.brokerSubs.set(obj, objSubs = new Set().add(cb))
 	}
 
-	useInsertionEffect(cleanup)
-
-	// due to the asynchronous nature of rendering
-	// useInsertionEffect may not always be called after each render
-	// (for ex. when the data was updated between the render and commit stages)
-	// we will schedule cleanup so as not to miss such a situation
-	queueMicrotask(cleanup)
+	return () => (
+		objSubs.delete(cb)
+	)
 }
